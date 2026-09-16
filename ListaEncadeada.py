c07add3ee0d9b71e35f4cf7e1434b842a893f43b
Ulisses Vedovello Macedo <ulissesvedovello30@gmail.com>
2026-09-16 09:39:44 -0300

    Novos updates

--- ListaEncadeada.py
+++ ListaEncadeada.py
@@ -1,3 +1,29 @@
+from Node import Node
 class ListaEncadeada:
     def __init__(self):
         self.__head = None
+
+
+    def inserir(self, dado):
+        if self.__head is None:
+            self.__head = Node(dado)
+        else:
+            atual = self.__head
+            while atual.get_apontar_o_proximo() is not None:
+                atual = atual.get_apontar_o_proximo()
+            atual.set_apontar_o_proximo(Node(dado))
+
+    def remover(self, dado):
+        if self.__head is None:
+            return
+        elif self.__head.get_dados() == dado:
+            self.__head = self.__head.get_apontar_o_proximo()
+        else:
+            anterior = self.__head
+            atual = self.__head.get_apontar_o_proximo()
+            while atual is not None:
+                if atual.get_dados() == dado:
+                    anterior.set_apontar_o_proximo(atual.get_apontar_o_proximo())
+                    return
+                anterior = atual
+                atual = atual.get_apontar_o_proximo()
--- Node.py
+++ Node.py
@@ -1,6 +1,23 @@
-class node:
+class Node:
     # O node vai precisar apenas dos "dados" e o ponteiro para apontar para o proximo
     # a maioria das classes vão utilizar a classe Node
     def __init__(self,dados):
         self.__dados = dados
         self.__apontar_o_proximo = None
+
+# Foi necessário criar esse geters e setters, pois a classe ListaEncadeada não iria conseguir
+# usar os dados da classe Node sem quebrar a aplicação por causa do encapsulamento das variaveis.
+
+    def get_dados(self):
+        return self.__dados
+
+    def set_dados(self, new_dados):
+        self.__dados = new_dados
+
+    def get_apontar_o_proximo(self):
+        return self.__apontar_o_proximo
+
+    def set_apontar_o_proximo(self, new_apontar_o_proximo):
+        self.__apontar_o_proximo = new_apontar_o_proximo
+
+
